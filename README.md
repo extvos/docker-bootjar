c3a572ec4dd944e73b401ccd8ac9f2a476a783ea
Mingcai SHEN <archsh@gmail.com>
2020-09-04 17:15:30 +0800

    Updated: change to display usage by default.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # extvos/bootjar
 
 Boot JAR executable package, normally for SpringBoot JAR
+
+Usage:
+        Bind jar file to /webapps/boot.jar
+        Default expose port is 8080
+Envronments:
+        JAVA_OPTS: java options, default is -Xms128M -Xmx256M
+        BOOT_ARGS: application arguments, default is empty.
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,21 @@
 
 set -e
 
+usage() {
+	echo "#### Docker Image for Boot JAR directly"
+	echo "Usage:  "
+	echo "        Bind jar file to /webapps/boot.jar"
+	echo "        Default expose port is 8080"
+	echo "Envronments:"
+	echo "        JAVA_OPTS: java options, default is ${JAVA_OPTS}"
+	echo "        BOOT_ARGS: application arguments, default is empty."
+	echo ""
+}
 
 if [ $# -gt 0 ]; then
 	exec "$@"
 elif [ -f /webapps/boot.jar ]; then
 	java ${JAVA_OPTS} -jar /webapps/boot.jar ${BOOT_ARGS}
 else
-	bash
+	usage
 fi
